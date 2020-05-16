ea3a7cb110baf9b90d94da9be8fe428dfff87c4b
sleepyarpan <arpanchattopadhyay23@gmail.com>
2020-05-16 20:54:06 +0530

    Cooking list added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React, { useState } from 'react';
-import Counter from './Counter';
-import CounterHooks from './CounterHooks';
-
-export const ThemeContext = React.createContext();
+import React from 'react';
+import RecipeList from './RecipeList';
 
 function App() {
-  console.log('Render App');
-  const [theme, setTheme] = useState('green');
-  return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      Counter using Class based component
-      <Counter initialCount={0} />
-      Counter using Functional component
-      <CounterHooks initialCount={0} />
-      <button
-        onClick={() =>
-          setTheme((prevTheme) => {
-            return prevTheme === 'red' ? 'blue' : 'red';
-          })
-        }
-      >
-        Toggle Theme
-      </button>
-    </ThemeContext.Provider>
-  );
+  return <RecipeList recipes={sampleRecipes} />;
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions:
+      '1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken',
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: '1. Put paprika on pork\n2. Put pork in oven\n3. Eat pork',
+  },
+];
 
 export default App;
