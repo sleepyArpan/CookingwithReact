e938b190b57d73efadbd9c7aabc3f0e64aad42f4
sleepyarpan <arpanchattopadhyay23@gmail.com>
2020-05-16 17:17:04 +0530

    Classbased component

---
+++ src/Counter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.initialCount,
+    };
+  }
+
+  changeCounter(amount) {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + amount,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.changeCounter(-1)}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={() => this.changeCounter(1)}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
