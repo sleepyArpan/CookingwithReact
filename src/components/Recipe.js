b610d3610d4c5f221511efc2391213887a0ab9ff
sleepyarpan <arpanchattopadhyay23@gmail.com>
2020-05-17 10:44:10 +0530

    Delete functionality added to Context

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,16 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import IngredientList from './IngredientList';
+import { RecipeContext } from './App';
 
 export default function Recipe(props) {
-  const {
-    id,
-    name,
-    cookTime,
-    servings,
-    instructions,
-    ingredients,
-    handleRecipeDelete,
-  } = props;
+  const { handleRecipeDelete } = useContext(RecipeContext);
+
+  const { id, name, cookTime, servings, instructions, ingredients } = props;
   return (
     <div className='recipe'>
       <div className='recipe__header'>
