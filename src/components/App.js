e93a9e45c4a0e8aaec7ebbfb8a877c1fbcdde1b7
sleepyarpan <arpanchattopadhyay23@gmail.com>
2020-05-17 10:09:19 +0530

    Recipe add and delete button prop drilling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RecipeList from './RecipeList';
 import '../css/app.css';
+import uuidv4 from 'uuid/v4';
 
 function App() {
-  return <RecipeList recipes={sampleRecipes} />;
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [
+        {
+          id: uuidv4(),
+          name: 'Name',
+          amount: '1 tbs',
+        },
+      ],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  }
+
+  return (
+    <RecipeList
+      recipes={recipes}
+      handleRecipeAdd={handleRecipeAdd}
+      handleRecipeDelete={handleRecipeDelete}
+    />
+  );
 }
 
 const sampleRecipes = [
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Recipe from './Recipe';
 
-export default function RecipeList({ recipes }) {
+export default function RecipeList(props) {
+  const { recipes, handleRecipeAdd, handleRecipeDelete } = props;
   return (
     <div className='recipe-list'>
       <div>
         {recipes.map((recipe) => {
-          return <Recipe key={recipe.id} {...recipe} />;
+          return (
+            <Recipe
+              key={recipe.id}
+              {...recipe}
+              handleRecipeDelete={handleRecipeDelete}
+            />
+          );
         })}
       </div>
       <div className='recipe-list__add-recipe-btn-container'>
-        <button className='btn btn--primary'>Add Recipe</button>
+        <button className='btn btn--primary' onClick={handleRecipeAdd}>
+          Add Recipe
+        </button>
       </div>
     </div>
   );
